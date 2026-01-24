51e0ad1859138c57e37745abff7c596084335b6a
Dan Josh <95506168+ddjoshua01@users.noreply.github.com>
2026-01-24 02:10:24 -0700

    bumper detection

---
+++ src/main/java/frc/robot/subsystems/BumperDetection.java
@@ -0,0 +1,70 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.constants.FieldConstants;
+import frc.robot.constants.VisionConstants;
+
+import java.util.Optional;
+
+public class BumperDetection extends SubsystemBase {
+
+    private SwerveSubsystem swerveSubsystem;
+
+    private final NetworkTable limelight = NetworkTableInstance.getDefault().getTable("limelight");
+
+    private Pose3d getAdjustedCameraPose() {
+        return new Pose3d(
+
+                VisionConstants.BALL_CAM_POSE_X + Units.inchesToMeters(VisionConstants.BALL_CAM_ADJUST_X.get()),
+                VisionConstants.BALL_CAM_POSE_Y + Units.inchesToMeters(VisionConstants.BALL_CAM_ADJUST_Y.get()),
+                VisionConstants.BALL_CAM_POSE_Z + Units.inchesToMeters(VisionConstants.BALL_CAM_ADJUST_Z.get()),
+
+                new Rotation3d(
+                        Units.degreesToRadians(VisionConstants.BALL_CAM_POSE_ROLL + VisionConstants.BALL_CAM_ADJUST_ROLL.get()),
+                        Units.degreesToRadians(VisionConstants.BALL_CAM_POSE_PITCH + VisionConstants.BALL_CAM_ADJUST_PITCH.get()),
+                        Units.degreesToRadians(VisionConstants.BALL_CAM_POSE_YAW + VisionConstants.BALL_CAM_ADJUST_YAW.get())));
+
+    }
+
+    public void setPipeline(int pipeline) {
+        limelight.getEntry("pipeline").setDouble(pipeline);
+    }
+
+    public boolean seeBumpers() {
+
+        boolean hasTarget = limelight.getEntry("tv").getDouble(0) == 1;
+        Pose3d cameraPose = getAdjustedCameraPose();
+
+        if (!hasTarget) {
+            return false;
+        } else {
+            Optional<DriverStation.Alliance> allianceOpt = DriverStation.getAlliance();
+
+            if (allianceOpt.isEmpty()) {
+                return false;
+            }
+
+
+            DriverStation.Alliance alliance = allianceOpt.get();
+            int pipeline = (alliance == DriverStation.Alliance.Red)
+                    ? 0
+                    : 1;
+
+            this.setPipeline(pipeline);
+            double ty = limelight.getEntry("ty").getDouble(0);
+
+            return ty >= VisionConstants.BUMPER_DETECTION_RETRACT_TY_DEGREES;
+
+        }
+
+    }
+
+}
