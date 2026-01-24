99e4fd3a4d46e5811d628b9af6234ab3c3c79d7f
Frederik <Frederik@schabel.com>
2026-01-24 14:01:16 -0700

    Formatted it into two main function that return turret angle and hood angle

--- src/main/java/frc/robot/commands/AimAtHub.java
+++ src/main/java/frc/robot/commands/AimAtHub.java
@@ -19,7 +19,7 @@ public class AimAtHub extends Command {
 
 
 
-    public AimAtHub(SwerveSubsystem swerveSubsystem, Pose2d currentPose, Pose3d targetPose, Pose2d shooterVelocity, TurretSubsystem turret) {
+    public AimAtHub(SwerveSubsystem swerveSubsystem, RobotState robotState, Pose2d currentPose, Pose3d targetPose, Pose2d shooterVelocity, TurretSubsystem turret) {
         this.swerveSubsystem = swerveSubsystem;
         this.robotState = robotState;
         this.currentPose = currentPose;
@@ -36,11 +36,11 @@ public class AimAtHub extends Command {
         return Math.hypot(pose.getX(), pose.getY());
     }
 
-    public double turretRotationAngel(double turretAngle, double robotAngle, double targetAngle) {
+    private double turretRotationAngel(double turretAngle, double robotAngle, double targetAngle) {
         return turretAngle * -1 + robotAngle * -1 +  targetAngle;
     }
 
-    public Pose2d getRelative2d(Pose2d pose0, Pose2d pose1,  Pose2d pose2, double t, int n, double vMag) {
+    private Pose2d getRelative2d(Pose2d pose0, Pose2d pose1,  Pose2d pose2, double t, int n, double vMag) {
         Pose2d pose = new Pose2d( new Translation2d((pose0.getX()-(pose1.getX() + pose2.getX() * t)),(pose0.getY()-(pose1.getY() + pose2.getY() * t))), pose0.getRotation());
         for(int i = 0; i < n; i++) {
             t = distance(pose)/vMag;
@@ -48,7 +48,7 @@ public class AimAtHub extends Command {
         }
         return pose;
     }
-    public Pose3d getRelative3d(Pose3d pose0, Pose2d pose1,  Pose2d pose2, double t, int n, double vMag) {
+    private Pose3d getRelative3d(Pose3d pose0, Pose2d pose1,  Pose2d pose2, double t, int n, double vMag) {
         Pose3d pose = new Pose3d( new Translation3d((pose0.getX()-(pose1.getX() + pose2.getX() * t)),(pose0.getY()-(pose1.getY() + pose2.getY() * t)), pose0.getZ()), pose0.getRotation());
         for(int i = 0; i < n; i++) {
             t= distance(pose.toPose2d())/vMag;
@@ -57,11 +57,11 @@ public class AimAtHub extends Command {
         return pose;
     }
 
-    public Pose2d subtract(Pose2d pose0, Pose2d pose1) {
+    private Pose2d subtract(Pose2d pose0, Pose2d pose1) {
         return new Pose2d(new Translation2d(pose0.getX()-pose1.getX(), pose0.getY()-pose1.getY()), pose0.getRotation());
     }
 
-    public double shootingAngle(Pose3d relitivePos, double v, double t){
+    private double shootingAngle(Pose3d relitivePos, double v, double t){
         double dx = relitivePos.getX();
         double dy = relitivePos.getY();
         double dz = relitivePos.getZ();
@@ -74,9 +74,28 @@ public class AimAtHub extends Command {
         double x = (d) / (v*t);
         return Math.atan2(x,z);
     }
+    
+    public double turretAngle(){
+        Pose2d targetPos2d = targetPose.toPose2d();
+        Pose2d robotVelocity = new Pose2d(
+                new Translation2d(
+                        swerveSubsystem.getFieldVelocity().vxMetersPerSecond,
+                        swerveSubsystem.getFieldVelocity().vyMetersPerSecond),
+                new Rotation2d(swerveSubsystem.getFieldVelocity().omegaRadiansPerSecond));
 
-    @Override
-    public void execute() {
+        Pose2d horizontalVector = subtract(targetPos2d, currentPose);
+        double vMag = distance(shooterVelocity);
+        double time = distance(horizontalVector)/vMag;
+
+        Pose2d relativePose2d = getRelative2d(targetPos2d, currentPose, robotVelocity , time, 3, vMag);
+        double targetAngle = angel(relativePose2d);
+        if(targetAngle < 0){
+            targetAngle = Math.PI*2 + targetAngle;
+        }
+        return targetAngle;
+    }
+    
+    public double hoodAngle(){
         Pose2d targetPos2d = targetPose.toPose2d();
         Pose2d robotVelocity = new Pose2d(
                 new Translation2d(
@@ -92,12 +111,8 @@ public class AimAtHub extends Command {
         Pose3d relativePose3d = getRelative3d(targetPose, currentPose, robotVelocity, time, 3, vMag);
         double d = distance(relativePose2d);
         time = d/vMag;
-        double targetAngle = angel(relativePose2d);
-        if(targetAngle < 0){
-            targetAngle = Math.PI*2 + targetAngle;
-        }
-        turret.setTurret(targetAngle + currentPose.getRotation().getRadians() * -1);
         double shooterAngle = shootingAngle(relativePose3d, vMag, time);
+        return shooterAngle;
     }
 
 
