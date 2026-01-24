0fd19df08249cee5df14957362951e523f6a04d1
frederikschabel <frederik@schabel.com>
2026-01-24 10:57:05 -0700

    Added Poses for Hub, Depot and Feeder

--- src/main/java/frc/robot/constants/FieldConstants.java
+++ src/main/java/frc/robot/constants/FieldConstants.java
@@ -1,7 +1,9 @@
 package frc.robot.constants;
 
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.util.Units;
 
 public class FieldConstants {
@@ -14,10 +16,13 @@ public class FieldConstants {
 
     // Garage bullshit values
 
-    public static final Pose2d ALGAE_1_LOLLIPOP_POINT = new Pose2d(1.24, 3.64, new Rotation2d(2.74));
-    public static final Pose2d ALGAE_2_LOLLIPOP_POINT = new Pose2d(1.33, 2.79, new Rotation2d(-2.443));
-    public static final Pose2d SHOOT_BARGE_POINT = new Pose2d(3, 3, new Rotation2d(0));
-
+    public static final Pose3d RED_HUB_POSE3D = new Pose3d(4.62534,4.034663,1.822, new Rotation3d(0,0,0));
+    public static final Pose3d BLUE_HUB_POSE3D = new Pose3d(4.62534 + 7.2898,4.034663,1.822, new Rotation3d(0,0,0));
+    public static final Pose2d BOTTOM_LEFT_CORNER_DEPOT = new Pose2d(0, 4.924044, new Rotation2d(0,0));
+    public static final Pose2d BOTTOM_RIGHT_CORNER_DEPOT = new Pose2d(0.6858,4.924044, new Rotation2d(0,0));
+    public static final Pose2d TOP_LEFT_CORNER_DEPOT = new Pose2d(0,7.002526, new Rotation2d(0,0));
+    public static final Pose2d TOP_RIGHT_CORNER_DEPOT = new Pose2d(0.6858,7.002526, new Rotation2d(0,0));
+    public static final Pose2d FEEDER_STATION = new Pose2d(0,0.632968, new Rotation2d(0,0));
     // 1 is center face from drive POV then go clockwise w/ driver POV
 
     // Should just be ID 10, 5, 2 pose
