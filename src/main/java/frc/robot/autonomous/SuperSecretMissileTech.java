f315e04d6307ccb97323458f8245d8b976bf999f
Dan Josh <95506168+ddjoshua01@users.noreply.github.com>
2026-02-05 10:34:11 -0700

    add depot shoot auto

--- src/main/java/frc/robot/autonomous/SuperSecretMissileTech.java
+++ src/main/java/frc/robot/autonomous/SuperSecretMissileTech.java
@@ -2,16 +2,19 @@ package frc.robot.autonomous;
 
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.autonomous.routines.DepotShoot;
 import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
 import frc.robot.subsystems.SwerveSubsystem;
+import swervelib.SwerveDrive;
 
 public class SuperSecretMissileTech {
 
     private final SendableChooser<SequentialCommandGroup> superSecretMissileTech = new SendableChooser<>();
 
-    public SuperSecretMissileTech(SwerveSubsystem swerveSubsystem, IntakeSubsystem intakeSubsystem, ShooterSubsystem shooterSubsystem) {
+    public SuperSecretMissileTech(SwerveSubsystem swerveSubsystem, SwerveDrive swerveDrive, IntakeSubsystem intakeSubsystem, ShooterSubsystem shooterSubsystem) {
         superSecretMissileTech.setDefaultOption("NOTHING", new SequentialCommandGroup());
+        superSecretMissileTech.addOption("DEPOT_SHOOT", new DepotShoot(swerveSubsystem, swerveDrive));
     }
 
     public SequentialCommandGroup getSelected() {
